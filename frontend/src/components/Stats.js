0113bf0ece2e0ed467fbf67059977535e6f8f945
Tomás Antunes <tomasantunes@gmail.com>
2023-10-28 19:57:29 +0100

    Create stats page

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar';
 
 export default function Stats() {
+  const [xp, setXp] = useState(0);
+  const [level, setLevel] = useState(0);
+  const [nrSkills, setNrSkills] = useState(0);
+  const [nrGoalsCompleted, setNrGoalsCompleted] = useState(0);
+
+  function loadStats() {
+
+  }
+
+  useEffect(() => {
+    loadStats();
+  }, [])
   return (
     <>
       <Navbar />
-      <div>Stats</div>
+      <div className="small-container">
+        <h2>Stats</h2>
+        <p><b>XP: </b>{xp}</p>
+        <p><b>Level: </b>{level}</p>
+        <p><b>Nr. Of Skills: </b>{nrSkills}</p>
+        <p><b>Goals completed: </b>{nrGoalsCompleted}</p>
+      </div>
     </>
     
   )
