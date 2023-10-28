4c4b46d3c3bd5c746649460fe64088ee2bdff717
Tomás Antunes <tomasantunes@gmail.com>
2023-10-28 13:59:40 +0100

    Create home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Loading.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Tasks from './components/Tasks';
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from './Navbar';
 
 export default function Home() {
+  const [quest, setQuest] = useState('');
+  const [questResponse, setQuestResponse] = useState('');
+  const [showQuest, setShowQuest] = useState(false);
+  const [showReport, setShowReport] = useState(false);
+  const [showQuestResponse, setShowQuestResponse] = useState(false);
+  const [loading, setLoading] = useState(false);
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <div className="container">
+        <h2>Home</h2>
+        <div style={{textAlign: "center"}}>
+          <button className="btn btn-primary">Start</button>
+        </div>
+        {showQuest &&
+          <div className="quest">
+            {{quest}}
+          </div>
+        }
+        {showReport &&
+          <div className="report">
+            <form>
+              <div className="form-group">
+                <label>Action</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label>Report</label>
+                <textarea className="form-control" rows="5"></textarea>
+              </div>
+              <div className="form-group">
+                <label>XP</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div style={{textAlign: "right"}}>
+                <button className="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        }
+        {showQuestResponse &&
+          <div className="quest-response">
+            {{questResponse}}
+          </div>
+        }
+      </div>
+      {loading &&
+        <div className="loading">Loading&#8230;</div>
+      }
     </>
     
   )
