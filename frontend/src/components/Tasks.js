a9b6790658f3c7119f1c4a24c2c9cd6ba9c55cf7
Tomás Antunes <tomasantunes@gmail.com>
2023-10-28 15:53:22 +0100

    Create tasks page

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar';
+import Select from 'react-select';
 
 export default function Tasks() {
+  const [goals, setGoals] = useState([]);
+  const [selectedGoal, setSelectedGoal] = useState();
+  const [tasks, setTasks] = useState([]);
+  const [selectedTaskType, setSelectedTaskType] = useState();
+
+  const taskTypes = [
+    {value: "single", label: "Single"},
+    {value: "recurrent", label: "Recurrent"}
+  ];
+
+  function changeSelectedGoal(goal) {
+
+  }
+
+  function changeSelectedTaskType(taskType) {
+
+  }
+
+  function loadGoals() {
+
+  }
+
+  function addTask() {
+
+  }
+
+  useEffect(() => {
+    loadGoals();
+  }, [])
   return (
     <>
       <Navbar />
-      <div>Tasks</div>
+      
+      <div className="small-container">
+        <h2>Tasks</h2>
+        <Select options={goals} value={selectedGoal} onChange={changeSelectedGoal} className="my-2" />
+        <table className="table table-striped table-bordered align-middle tasks">
+          <thead class="table-dark">
+              <tr>
+                  <th>Task</th>
+                  <th>Type</th>
+                  <th>Options</th>
+              </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr>
+                <td>{task.description}</td>
+                <td>{task.type}</td>
+                <td></td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <input type="text" className="form-control" placeholder="Add a new task" />
+              </td>
+              <td>
+                <Select options={taskTypes} value={selectedTaskType} onChange={changeSelectedTaskType} />
+              </td>
+              <td>
+                <button className="btn btn-success" onClick={addTask}>Add</button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </>
   )
 }
