e3ab16ff4da56fddd7f395b6c58c418f0135f169
Tomás Antunes <tomasantunes@gmail.com>
2023-10-28 12:19:09 +0100

    Update

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cors = require('cors');
+var mysql = require('mysql');s
 
 var app = express();
 
@@ -18,9 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
