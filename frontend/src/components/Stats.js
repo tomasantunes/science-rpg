110826088038305a3e34ff782237362e776a96f2
Tomás Antunes <tomasantunes@gmail.com>
2023-10-28 20:17:21 +0100

    Add field to stats

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -12,6 +12,7 @@ export default function Stats() {
   const [level, setLevel] = useState(0);
   const [nrSkills, setNrSkills] = useState(0);
   const [nrGoalsCompleted, setNrGoalsCompleted] = useState(0);
+  const [nrItems, setNrItems] = useState(0);
 
   function loadStats() {
 
@@ -29,6 +30,7 @@ export default function Stats() {
         <p><b>Level: </b>{level}</p>
         <p><b>Nr. Of Skills: </b>{nrSkills}</p>
         <p><b>Goals completed: </b>{nrGoalsCompleted}</p>
+        <p><b>Nr. Of Items: </b>{nrItems}</p>
       </div>
     </>
     
