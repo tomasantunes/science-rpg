cfb4e16361e2851120625759964ab9603cfb8539
Tomás Antunes <tomasantunes@gmail.com>
2023-10-28 19:54:25 +0100

    Create skills page

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from './Navbar';
 
 export default function Skills() {
+  const [skills, setSkills] = useState([]);
+
+  function loadSkills() {
+
+  }
+
+  function addSkill() {
+
+  }
+
+  useEffect(() => {
+    loadSkills();
+  }, [])
   return (
     <>
       <Navbar />
-      <div>Skills</div>
+      <div className="small-container">
+        <h2>Skills</h2>
+        <table className="table table-striped table-bordered align-middle tasks">
+          <thead class="table-dark">
+              <tr>
+                  <th>Skill</th>
+                  <th>Progress</th>
+              </tr>
+          </thead>
+          <tbody>
+            {skills.map((skill) => (
+              <tr>
+                <td>{skill.skill_name}</td>
+                <td>{skill.percentage}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <input type="text" className="form-control" placeholder="Add a new skill" />
+              </td>
+              <td>
+                <button className="btn btn-success" onClick={addSkill}>Add</button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </>
-   
+    
   )
 }
